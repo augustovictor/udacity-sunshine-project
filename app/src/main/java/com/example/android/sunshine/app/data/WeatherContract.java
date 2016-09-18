b8b7222cf4a69431be5439850614c698a8287aa1
Victor <victoraweb@gmail.com>
2016-09-18 13:56:18 -0300

    type: test Location table and tests;

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -41,6 +41,18 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        // Location setting
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // Latitude
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+
+        // Longitude
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
+        // City Name
+        public static final String COLUMN_CITY_NAME = "city_name";
+
     }
 
     /* Inner class that defines the table contents of the weather table */
