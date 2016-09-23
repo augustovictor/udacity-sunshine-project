d1db654c512923922b4e0d52b76e72d9d298dc8e
Victor <victoraweb@gmail.com>
2016-09-23 10:11:13 -0300

    type: feat Handling different view types;

--- app/src/main/java/com/example/android/sunshine/app/adapters/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/adapters/ForecastAdapter.java
@@ -18,8 +18,23 @@ import com.example.android.sunshine.app.utils.Utility;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
+    private final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
     }
 
     /**
@@ -54,7 +69,22 @@ public class ForecastAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
 
-        return view;
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        switch (viewType) {
+            case VIEW_TYPE_TODAY: {
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            }
+
+            case VIEW_TYPE_FUTURE_DAY: {
+                layoutId = R.layout.list_item_forecast;
+                break;
+            }
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
